6793de9d74fb3d4c646ca102dd54e56831261d33
s6352410016 <s6352410016@sau.ac.th>
2024-03-22 20:54:10 +0700

    Add conversation ui

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -3,8 +3,16 @@
 import useRoutes from "@/app/hooks/useRoutes";
 import React, { useState } from "react";
 import DesktopItem from "./DesktopItem";
+import { User } from "@prisma/client";
+import Avatar from "../Avatar";
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps {
+    currentUser: User;
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
+    currentUser
+}) => {
     const routes = useRoutes();
     const [isOpen, setIsOpen] = useState(false);
 
@@ -54,6 +62,26 @@ const DesktopSidebar = () => {
                         />
                     ))}
                 </ul>
+            </nav>
+            <nav
+                className="
+                    mt-4
+                    flex
+                    flex-col
+                    justify-between
+                    items-center
+                "
+            >
+                <div
+                    onClick={() => setIsOpen(true)}
+                    className="
+                        cursor-pointer
+                        hover:opacity-75
+                        transition
+                    "
+                >
+                    <Avatar user={currentUser} />
+                </div>
             </nav>
         </div>
     );
